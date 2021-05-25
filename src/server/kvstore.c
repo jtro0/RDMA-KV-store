703dea15116d8dfe81948bdec1722f751f800c4a
jtro0 <j.e.troost@student.vu.nl>
2021-05-25 17:26:56 +0200

    print actual ud request count

--- src/server/kvstore.c
+++ src/server/kvstore.c
@@ -90,7 +90,7 @@ void *main_job(void *arg) {
         ready_for_next_request(client);
 
         bzero(client->response, sizeof(struct response));
-        pr_info("client %d: request count %d\n", client->client_nr, client->request_count);
+        pr_info("client %d: request count %d\n", client->client_nr, client->ud_client->ud_server->request_count);
         switch (request->method) {
             case SET:
                 pr_info("set\n");
