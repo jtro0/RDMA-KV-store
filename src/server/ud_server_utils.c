db77a8f9c37a3b2fe2b9d13569f5aef4d7717da9
Jens <j.e.troost@student.vu.nl>
2021-05-31 11:41:21 +0200

    time to debug seg fault

--- src/server/ud_server_utils.c
+++ src/server/ud_server_utils.c
@@ -266,7 +266,7 @@ int ud_receive_header(struct client_info *client) {
 //    sleep(5);
     pr_info("client %d: going to poll for recv\n", client->client_nr);
     bzero(client->ud_client->wc, sizeof(struct ibv_wc));
-    client->ud_client->wc->wc_flags = IBV_WC_GRH;
+//    client->ud_client->wc->wc_flags = IBV_WC_GRH;
 //    print_request(&client->ud_client->ud_server->request[client->ud_client->ud_server->request_count].request);
 
     ret = process_work_completion_events(client->ud_client->ud_server->io_completion_channel_recv, client->ud_client->wc, 1, client->ud_client->ud_server->ud_recv_cq);
@@ -294,7 +294,7 @@ int ud_send_response(struct client_info *client) {
     int ret = -1;
     struct ibv_wc wc;
 
-    pr_info("sending to client %d\n", client->ud_client->client_handling);
+    printf("sending to client %d\n", client->ud_client->client_handling);
     ret = ud_post_send(sizeof(struct response), client->ud_client->response_mr->lkey, 0, client->ud_client->ud_server->ud_qp, client->response,
                         client->ud_client->ud_server->ah[client->ud_client->client_handling], client->ud_client->ud_server->remote_dgram_qp_attrs[client->ud_client->client_handling].qpn);
     ret = process_work_completion_events(client->ud_client->ud_server->io_completion_channel_send, &wc, 1, client->ud_client->ud_server->ud_send_cq);
