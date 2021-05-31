c95eff6ed08a6fa1c0f7af4116dc7c956dad2ae2
Jens <j.e.troost@student.vu.nl>
2021-05-31 11:44:45 +0200

    time to debug seg fault

--- src/rdma_common.c
+++ src/rdma_common.c
@@ -290,7 +290,7 @@ int ud_post_send(size_t size, uint32_t lkey, uint64_t wr_id, struct ibv_qp *qp, 
             .wr.ud.remote_qkey = 0x11111111
     };
 
-    printf("send %p %p %p %p %p", buf, &send_wr, &bad_send_wr, qp, ah);
+    fprintf(stderr, "send %p %p %p %p %p\n", buf, &send_wr, &bad_send_wr, qp, ah);
 
     ret = ibv_post_send(qp, &send_wr, &bad_send_wr);
     return ret;
